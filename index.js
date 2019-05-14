372da51587aa5b7e23c7a8fdd3313a0bc549cdb3
gonnabea <48288590+gonnabea@users.noreply.github.com>
2019-05-15 01:42:15 +0900

    node,express,babel

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("Hi!");
+import express from "express";
+const app = express();
+
+const PORT = 4000;
+
+const handleListening = () =>
+    console.log("Listening on: http://localhost:4000");
+
+
+const handleHome = (req, res) =>
+    res.send("Hello from my ass");
+
+
+const handleProfile = (req, res) => res.send("You are on my profile.");
+
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
+
+app.listen(PORT, handleListening);
