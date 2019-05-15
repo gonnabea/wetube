dd40cfd4fa9b624f90ce95b1c64672908c46cd05
gonnabea <48288590+gonnabea@users.noreply.github.com>
2019-05-16 01:58:15 +0900

    middlewares

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
 const app = express();
 
 const PORT = 4000;
 
 const handleListening = () =>
-    console.log("Listening on: http://localhost:4000");
+    console.log(`Listening on: http://localhost:${PORT}`);
 
 
 const handleHome = (req, res) =>
@@ -13,8 +17,15 @@ const handleHome = (req, res) =>
 
 const handleProfile = (req, res) => res.send("You are on my profile.");
 
+app.use(cookieParser());
+app.use(bodyParser.json({extended : true }));
+app.use(bodyParser.urlencoded({extended : true }));
+app.use(helmet());
+app.use(morgan("dev"));
+
 app.get("/", handleHome);
 
 app.get("/profile", handleProfile);
+
 
 app.listen(PORT, handleListening);
