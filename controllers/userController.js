ba6e58aea2841003770517e89037337e750df763
gonnabea <48288590+gonnabea@users.noreply.github.com>
2019-05-27 19:13:08 +0900

    end of express js & mongoDb installed

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,37 @@
-export const join = (req, res) =>
+
+import routes from "../routes";
+export const getJoin = (req, res) =>
  res.render('join', {pageTitle: "Join"});
-export const login = (req, res) =>
- res.render('login', {pageTitle: "Login"});
-export const logout = (req, res) =>
- res.render('logout', {pageTitle: "Logout"});
-export const users = (req, res) =>
- res.render('users', {pageTitle: "Users"});
+
+
+export const postJoin = (req, res) => {
+    const {
+        body: { name, email, password, password2}
+    } = req;
+    if(password !== password2){
+        console.log(req.body);
+      res.status(400);
+      res.render('join', { pageTitle: "Join" });
+    } else {
+        console.log(req.body);
+        // To Do: Register User
+        // To Do: Log user in
+        res.redirect(routes.home);
+    }
+};
+
+export const getLogin = (req, res) =>
+ res.render('login', {pageTitle: "Log In"});
+export const postLogin = ( req, res) => {
+ res.redirect(routes.home);
+}
+export const logout = (req, res) => {
+ // To Do: Process Log Out
+ res.redirect(routes.home);
+}
+
+
+
 export const userDetail = (req, res) =>
  res.render('userDetail', {pageTitle: "UserDeatil"});
 export const editProfile = (req, res) =>
