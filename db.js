01e86d0c97d69fb2d65a11d5908cfdd9bd732d38
gonnabea <48288590+gonnabea@users.noreply.github.com>
2019-05-27 20:01:33 +0900

    connecting to Mongo DB

--- db.js
+++ db.js
@@ -1,54 +1,18 @@
-export const videos = [
-    {
-        id: 324393,
-        title: 'Video awesome',
-        description: 'This is something I love',
-        views: 24,
-        videoFile:"https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator:{
-            id: 121212,
-            name:"Nicolas",
-            email:"nico@las.com"
+import mongoose from "mongoose";
 
-        }
-    },
-    {
-        id: 321432,
-        title: 'Video super',
-        description: 'This is something I love',
-        views: 24,
-        videoFile:"https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator:{
-            id: 121212,
-            name:"Nicolas",
-            email:"nico@las.com"
 
-        }
-    },
-    {
-        id: 122132,
-        title: 'Video nice',
-        description: 'This is something I love',
-        views: 24,
-        videoFile:"https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator:{
-            id: 121212,
-            name:"Nicolas",
-            email:"nico@las.com"
+mongoose.connect(
+    "mongodb://localhost:27017/we-tube",
+{
+    useNewUrlParser: true,
+    useFIndAndModify: false
+}
+);
 
-        }
-    },
-    {
-        id: 999999,
-        title: 'Video perfect',
-        description: 'This is something I love',
-        views: 24,
-        videoFile:"https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator:{
-            id: 121212,
-            name:"Nicolas",
-            email:"nico@las.com"
+const db = mongoose.connection;
 
-        }
-    }
-];
+const handleOpen =() => console.log("Connected to DB")
+const handleError = () => console.log(`X Error on DB connection:$error`)
+
+db.once("open", handleOpen)
+db.on("error", handleError)
